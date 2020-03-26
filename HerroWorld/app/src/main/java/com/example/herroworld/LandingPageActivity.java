3e69c12ffc7b1b88bbfd7d255fbbdea174385597
NickKettler <nickakettler@gmail.com>
2020-03-26 00:00:08 -0400

    Added Info Activity Added transitions between landing and Info activities Changed default activity to landing page Updated Gitignore to ignore some machine specific files

---
+++ HerroWorld/app/src/main/java/com/example/herroworld/LandingPageActivity.java
@@ -0,0 +1,22 @@
+package com.example.herroworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
+
+    public void openInfo(View view) {
+        Intent intent = new Intent(this, InfoActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_top, R.anim.slide_out_bottom);
+    }
+}
