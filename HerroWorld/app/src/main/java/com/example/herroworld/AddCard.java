bf88a9b240a6cbde3644bf46561dd934d020ca6e
NickKettler <nickakettler@gmail.com>
2020-04-01 21:40:35 -0400

    Images can uploaded from gallary

--- HerroWorld/app/src/main/java/com/example/herroworld/AddCard.java
+++ HerroWorld/app/src/main/java/com/example/herroworld/AddCard.java
@@ -1,14 +1,53 @@
 package com.example.herroworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class AddCard extends AppCompatActivity {
+public class AddCard extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    ImageView imageUpload;
+    EditText uploadedCardName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        imageUpload = (ImageView) findViewById(R.id.imageUpload);
+        uploadedCardName = (EditText) findViewById(R.id.cardNameField);
+
+        imageUpload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+    }
+
+    //Called when an image is selected from the gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageUpload.setImageURI(selectedImage);
+        }
+    }
+
+    @Override
+    public void finish(){
+        super.finish();
     }
 }
