3017273ea211c9758efcfd33c1b21239db0138a2
NickKettler <nickakettler@gmail.com>
2020-04-01 22:23:34 -0400

    Added button to finish uploading a single card

--- HerroWorld/app/src/main/java/com/example/herroworld/AddCard.java
+++ HerroWorld/app/src/main/java/com/example/herroworld/AddCard.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -17,6 +18,9 @@ public class AddCard extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageUpload;
     EditText uploadedCardName;
+    Button uploadButton;
+
+    boolean isImageUploaded;
 
 
     @Override
@@ -26,14 +30,24 @@ public class AddCard extends AppCompatActivity implements View.OnClickListener {
 
         imageUpload = (ImageView) findViewById(R.id.imageUpload);
         uploadedCardName = (EditText) findViewById(R.id.cardNameField);
+        uploadButton = (Button) findViewById(R.id.AddCardButton);
 
         imageUpload.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
+
+        isImageUploaded = false;
     }
 
     @Override
     public void onClick(View v) {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+        if(v == imageUpload){
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+        }
+        else {
+            this.finish();
+        }
+
     }
 
     //Called when an image is selected from the gallery
@@ -43,11 +57,17 @@ public class AddCard extends AppCompatActivity implements View.OnClickListener {
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
             Uri selectedImage = data.getData();
             imageUpload.setImageURI(selectedImage);
+            isImageUploaded = true;
         }
     }
 
+
     @Override
     public void finish(){
+        if(isImageUploaded && uploadedCardName != null){
+
+        }
         super.finish();
+
     }
 }
