88784ed87fa7fb05053a2b344682f2cf59555983
NickKettler <nickakettler@gmail.com>
2020-04-15 01:38:26 -0400

    Added Play activity Built play layout Imported placeholder Images for playControl icons

---
+++ SmartCards/app/src/main/java/com/example/SmartCards/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.example.SmartCards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PlayActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_screen);
+    }
+}
