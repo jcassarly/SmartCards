7997de36e23730a24cec032fc619888e4a445fe5
NickKettler <nickakettler@gmail.com>
2020-04-16 21:22:28 -0400

    DeckName is now read from memory and displayed on the play interface

--- SmartCards/app/src/main/java/com/example/SmartCards/PlayActivity.java
+++ SmartCards/app/src/main/java/com/example/SmartCards/PlayActivity.java
@@ -2,6 +2,7 @@ package com.example.SmartCards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,19 +10,24 @@ import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
 
-    TextView deckName, deckCount, inPlayCount, discardCount;
+    TextView deckNameText, deckCountText, inPlayCountText, discardCountText;
     Button exitButton;
+    private String deckName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
 
-        deckName = (TextView) findViewById(R.id.deckTitleText);
-        deckCount = (TextView) findViewById(R.id.deckCountText);
-        inPlayCount = (TextView) findViewById(R.id.inPlayCountText);
-        discardCount = (TextView) findViewById(R.id.discardCountText);
+        deckNameText = (TextView) findViewById(R.id.deckTitleText);
+        deckCountText = (TextView) findViewById(R.id.deckCountText);
+        inPlayCountText = (TextView) findViewById(R.id.inPlayCountText);
+        discardCountText = (TextView) findViewById(R.id.discardCountText);
         exitButton = (Button) findViewById(R.id.exitGameButton);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(EditDeck.SHARED_PREFS, MODE_PRIVATE);
+        deckName = sharedPreferences.getString(EditDeck.DECK_NAME,"DeckName");
+        deckNameText.setText(deckName);
     }
 
 
